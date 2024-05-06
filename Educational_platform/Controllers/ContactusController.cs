790cf8d9be76ea9ace59fe357cc53978c5e521b1
abdelrahmanamr54 <amrmmi22@gmail.com>
2024-05-07 00:33:51 +0300

    grade

---
+++ Educational_platform/Controllers/ContactusController.cs
@@ -0,0 +1,47 @@
+﻿using Educational_platform.Data;
+using Educational_platform.Models;
+using Educational_platform.Repository;
+using Educational_platform.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Educational_platform.Controllers
+{
+    public class ContactusController : Controller
+    {
+
+        private readonly ApplicationDbContext context;
+        public ContactusController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult addGrade(int id)
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult add_Grade(Contactus contactus)
+        {
+
+            context.contactus.Add(contactus);
+            context.SaveChanges();
+
+
+           
+            return RedirectToAction("Index", "Home");
+        }
+        public IActionResult showAllContact()
+        {
+            var contact = context.contactus.ToList();
+
+            return View(contact);
+        }
+    }
+}
---
+++ Educational_platform/Repository/GradeRepository.cs
@@ -0,0 +1,83 @@
+﻿using Educational_platform.Data;
+using Educational_platform.IRepositery;
+using Educational_platform.Models;
+using Educational_platform.ViewModel;
+using System.Numerics;
+
+namespace Educational_platform.Repository
+{
+    public class GradeRepository : IGradeRepository
+    {
+        private readonly ApplicationDbContext context;
+        public GradeRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+
+
+
+        public void Create(Grade grade)
+        {
+            context.grades.Add(grade);
+            context.SaveChanges();
+        }
+
+        public void Delete(Grade grade)
+        {
+            context.grades.Remove(grade);
+            context.SaveChanges();
+        }
+
+        public Grade  FindById(int id)
+        {
+            return context.grades.Find(id);
+        }
+        public List<Lecture> GetLecturerById(int LId)
+        {
+            var Lecture = context.lectures.Where(e => e.GradeId == LId).ToList();
+
+            return Lecture;
+        }
+        public List<Book> GetBookById(int BId)
+        {
+            var Book = context.books.Where(e => e.GradeId == BId).ToList();
+
+            return Book;
+        }
+        public List<StudentVM> GetstudentById(int sId)
+        {
+            var students = context.studentVMs.Where(e => e.GradeId == sId).ToList();
+
+            return students;
+        }
+
+
+        public List<Grade> ReadAll()
+
+        {
+            return context.grades.ToList();
+        }
+
+        public Grade findGrade(int id)
+        {
+            var oldgrade = context.grades.Find(id);
+
+            return oldgrade;
+
+        }
+
+        public Grade Update(Grade grade)
+        {
+            var oldgrade = context.grades.Find(grade.Id);
+            if (oldgrade != null)
+            {
+               oldgrade.Name = oldgrade.Name;
+              
+                context.SaveChanges();
+
+            }
+            return oldgrade;
+        }
+    }
+}
